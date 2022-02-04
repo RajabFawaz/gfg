aa6658be1ee6868de3e7c88aa93d69a869ce40ba
RajabFawaz <90001583+RajabFawaz@users.noreply.github.com>
2022-02-04 11:32:32 -0500

    Initial commit

---
+++ exercice1.py
@@ -0,0 +1,10 @@
+def fizzBuzz(n):
+    # TODO imprimer la chaine de caractère appropriée avec la fonction print().
+    #  Assigner ensuite la valeur à la variable resultat
+
+    resultat =
+    return resultat
+
+if __name__ == '__main__':
+    n = int(input("indiquez le nombre: "))
+    print(fizzBuzz(n))
